419356dc6df6011f2bf167d226410d074fc737ee
Abhishek Upmanyu <upmanyu.abhishek@yahoo.in>
2021-08-08 02:30:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ReactComponent as GithubIcon } from "./assets/images/github-icon.svg";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-50m.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          <ComposableMap>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} />
+                ))
+              }
+            </Geographies>
+          </ComposableMap>
+      </div>
+      <GithubIcon />
     </div>
   );
 }
