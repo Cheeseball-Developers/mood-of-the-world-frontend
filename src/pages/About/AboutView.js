bd36d736894de0a5a61eb24eade6e0a40b56b889
Abhishek Upmanyu <upmanyu.abhishek@yahoo.in>
2021-10-03 21:56:12 +0530

    Fix emotions image on about page of desktop view

--- src/pages/About/AboutView.js
+++ src/pages/About/AboutView.js
@@ -3,6 +3,7 @@ import { Grid, Typography } from '@material-ui/core';
 import { GithubProfileBadge } from './components/GithubProfileBadge';
 import { HighlightSection } from 'components';
 import emotions from 'assets/images/plutchik-emotions.png';
+import { isBrowser } from 'react-device-detect';
 
 export const AboutView = (props) => {
     return (
@@ -22,31 +23,38 @@ export const AboutView = (props) => {
             }
             />
             <br />
-            <Typography style={{ padding: '8px' }} variant='body1'>
-                The data is fed into the model from a real-time sampled stream from Twitter.
-                It focusses to classify emotion of a country, based on sentiment attached to them, with the aim to classify the tweets based on one or more of 11 emotions:
-                the eight Plutchik Categories:
-                <ul>
-                    <li>joy,</li>
-                    <li>sadness,</li>
-                    <li>anger,</li>
-                    <li>fear,</li>
-                    <li>trust,</li>
-                    <li>disgust,</li>
-                    <li>surprise,</li>
-                    <li>anticipation,</li>
-                </ul>
-                plus
-                <ul>
-                    <li>love,</li>
-                    <li>optimism,</li>
-                    <li>pessimism,</li>
-                </ul>
-                <img src={emotions} style={{ width: '100%' }} alt="Plutchik Emotions" />
-                <br />
-                Emotional investigation of Twitter data is a field that has been given much attention over the last decade and involves dissecting “tweets” (comments) and the content of these expressions.
-                We use NLP (Natural Language Processing) + Deep learning techniques to analyze sentiments based on 11 emotions.
-            </Typography>
+            <Grid container spacing={1}>
+                <Grid item xs={isBrowser ? 6 : 12}>
+                    <Typography style={{ padding: '8px' }} variant='body1'>
+                        The data is fed into the model from a real-time sampled stream from Twitter.
+                        It focusses to classify emotion of a country, based on sentiment attached to them, with the aim to classify the tweets based on one or more of 11 emotions:
+                        the eight Plutchik Categories:
+                        <ul>
+                            <li>joy,</li>
+                            <li>sadness,</li>
+                            <li>anger,</li>
+                            <li>fear,</li>
+                            <li>trust,</li>
+                            <li>disgust,</li>
+                            <li>surprise,</li>
+                            <li>anticipation,</li>
+                        </ul>
+                        plus
+                        <ul>
+                            <li>love,</li>
+                            <li>optimism,</li>
+                            <li>pessimism,</li>
+                        </ul>
+                        {!isBrowser ? <img src={emotions} style={{ width: '100%' }} alt="Plutchik Emotions" /> : <div></div>}
+                        <br />
+                        Emotional investigation of Twitter data is a field that has been given much attention over the last decade and involves dissecting “tweets” (comments) and the content of these expressions.
+                        We use NLP (Natural Language Processing) + Deep learning techniques to analyze sentiments based on 11 emotions.
+                    </Typography>
+                </Grid>
+                {isBrowser ? <Grid item xs={6}>
+                    <img src={emotions} style={{ width: '100%' }} alt="Plutchik Emotions" />
+                </Grid> : <div></div>}
+            </Grid>
             <br />
             <HighlightSection title='Contributors' child={
                 <Grid container spacing={1}>
